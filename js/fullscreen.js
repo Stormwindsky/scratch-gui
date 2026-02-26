ebf5886eb3b6675ad8943a31909d633631908883
Stormwindsky <83926426+Stormwindsky@users.noreply.github.com>
2026-02-26 22:14:57 +0000

    Deploying to gh-pages from @ Stormwindsky/scratch-gui@276f02bf03dfd6154132ef75fd3c85d152cf3ff4 🚀

--- js/fullscreen.js
+++ js/fullscreen.js
@@ -49333,7 +49333,7 @@ const handleClickAddonSettings = addonId => {
 const messages = Object(react_intl__WEBPACK_IMPORTED_MODULE_5__["defineMessages"])({
   defaultTitle: {
     "id": "tw.guiDefaultTitle",
-    "defaultMessage": "StormWarp - Lightning Fast Scratch"
+    "defaultMessage": "Lightning Fast Scratch"
   }
 });
 const WrappedMenuBar = Object(redux__WEBPACK_IMPORTED_MODULE_4__["compose"])(_lib_sb_file_uploader_hoc_jsx__WEBPACK_IMPORTED_MODULE_11__["default"], _lib_tw_packager_integration_hoc_jsx__WEBPACK_IMPORTED_MODULE_12__["default"])(_components_menu_bar_menu_bar_jsx__WEBPACK_IMPORTED_MODULE_16__["default"]);
