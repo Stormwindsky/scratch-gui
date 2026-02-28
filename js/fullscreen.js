9206d69b3cead4ba17de577f5cfbd8c18644b0f2
Stormwindsky <83926426+Stormwindsky@users.noreply.github.com>
2026-02-28 11:32:42 +0000

    Deploying to gh-pages from @ Stormwindsky/scratch-gui@cc7fa6788abcd1918d799686a93a0b1011ea135c 🚀

--- js/fullscreen.js
+++ js/fullscreen.js
@@ -49411,7 +49411,7 @@ const Footer = () => /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_2___default.a.c
 }))), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement("div", {
   className: _interface_css__WEBPACK_IMPORTED_MODULE_28___default.a.footerSection
 }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement("a", {
-  href: "https://scratch.mit.edu/users/GarboMuffin/#comments"
+  href: "https://gamejolt.com/c/Stormwindsky-yk7ybk/stormwarp_feedback"
 }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_2___default.a.createElement(react_intl__WEBPACK_IMPORTED_MODULE_5__["FormattedMessage"], {
   defaultMessage: "Feedback & Bugs",
   id: "tw.feedback"
