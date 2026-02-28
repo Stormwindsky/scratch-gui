e6225b6677cb0976938e31c6e07b1cb36aec0801
Stormwindsky <83926426+Stormwindsky@users.noreply.github.com>
2026-02-28 18:31:16 -0500

    Update tw-extension-tags.js

--- src/lib/libraries/tw-extension-tags.js
+++ src/lib/libraries/tw-extension-tags.js
@@ -3,5 +3,6 @@ import {APP_NAME} from '../brand';
 // Because there are all brand names, it is unnecessary for them to be translatable.
 export default [
     {tag: 'scratch', intlLabel: 'Scratch'},
+    {tag: 'tw', intlLabel: 'Turbowarp'},
     {tag: 'sw', intlLabel: APP_NAME}
 ];
