5c03e09f07e9773ef5132f5cd4c7d673d45e2e44
Stormwindsky <83926426+Stormwindsky@users.noreply.github.com>
2026-02-28 06:32:18 -0500

    Update settings.jsx

--- src/addons/settings/settings.jsx
+++ src/addons/settings/settings.jsx
@@ -1102,7 +1102,7 @@ class AddonSettingsComponent extends React.Component {
                             />
                         </div>
                         <a
-                            href="https://scratch.mit.edu/users/GarboMuffin/#comments"
+                            href="https://gamejolt.com/c/Stormwindsky-yk7ybk/stormwarp_feedback"
                             target="_blank"
                             rel="noreferrer"
                             className={styles.feedbackButtonOuter}
