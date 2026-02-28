acb1d8a9eaf6823419042781943c7577405d35b9
Stormwindsky <83926426+Stormwindsky@users.noreply.github.com>
2026-02-28 11:35:20 +0000

    Deploying to gh-pages from @ Stormwindsky/scratch-gui@5c03e09f07e9773ef5132f5cd4c7d673d45e2e44 🚀

--- js/addon-settings.js
+++ js/addon-settings.js
@@ -2225,7 +2225,7 @@ class AddonSettingsComponent extends react__WEBPACK_IMPORTED_MODULE_0___default.
       className: _settings_css__WEBPACK_IMPORTED_MODULE_18___default.a.searchButton,
       onClick: this.handleClickSearchButton
     })), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
-      href: "https://scratch.mit.edu/users/GarboMuffin/#comments",
+      href: "https://gamejolt.com/c/Stormwindsky-yk7ybk/stormwarp_feedback",
       target: "_blank",
       rel: "noreferrer",
       className: _settings_css__WEBPACK_IMPORTED_MODULE_18___default.a.feedbackButtonOuter
