cc7fa6788abcd1918d799686a93a0b1011ea135c
Stormwindsky <83926426+Stormwindsky@users.noreply.github.com>
2026-02-28 06:29:42 -0500

    Update render-interface.jsx

--- src/playground/render-interface.jsx
+++ src/playground/render-interface.jsx
@@ -158,7 +158,7 @@ const Footer = () => (
                     </a>
                 </div>
                 <div className={styles.footerSection}>
-                    <a href="https://scratch.mit.edu/users/GarboMuffin/#comments">
+                    <a href="https://gamejolt.com/c/Stormwindsky-yk7ybk/stormwarp_feedback">
                         <FormattedMessage
                             defaultMessage="Feedback & Bugs"
                             description="Link to feedback/bugs page"
