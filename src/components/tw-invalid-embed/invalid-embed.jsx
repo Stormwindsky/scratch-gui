1d930e616d602ab103063cd6a1ece58bd1207259
Stormwindsky <83926426+Stormwindsky@users.noreply.github.com>
2026-02-28 06:26:51 -0500

    Update invalid-embed.jsx

--- src/components/tw-invalid-embed/invalid-embed.jsx
+++ src/components/tw-invalid-embed/invalid-embed.jsx
@@ -35,7 +35,7 @@ const InvalidEmbed = () => (
         <p>
             {'If you are seeing this page even though you aren\'t embedding anything, let us know on '}
             <a
-                href="https://scratch.mit.edu/users/GarboMuffin/#comments"
+                href="https://gamejolt.com/c/Stormwindsky-yk7ybk/stormwarp_feedback"
                 target="_blank"
                 rel="noreferrer"
             >
