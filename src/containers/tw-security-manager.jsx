29c7695b73f1f681b9c25ca71914ae59ecbb2164
Stormwindsky <83926426+Stormwindsky@users.noreply.github.com>
2026-03-08 12:06:45 -0400

    Update tw-security-manager.jsx

--- src/containers/tw-security-manager.jsx
+++ src/containers/tw-security-manager.jsx
@@ -33,6 +33,7 @@ const isTrustedExtension = url => (
     url.startsWith('https://extensions.penguinmod.com/') ||
     url.startsWith('https://sharkpools-extensions.vercel.app/') ||
     url.startsWith('https://cdn.jsdelivr.net/gh/Stormwindsky/My-Extension-Blocks-under-license-CC0-1.0-for-Turbowarp@main/List/') ||
+    url.startsWith('https://ccw-user-extension.ccw.site/extensions/') ||
 
     // For development.
     url.startsWith('http://localhost:8000/') ||
