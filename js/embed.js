163b00a530b5905bebb0cecfd627dc516649045a
Stormwindsky <83926426+Stormwindsky@users.noreply.github.com>
2026-03-08 22:48:54 +0000

    Deploying to gh-pages from @ Stormwindsky/scratch-gui@1b8282e0aa26eb87297196f39ab3f30beb2c5ddb 🚀

--- js/embed.js
+++ js/embed.js
@@ -13283,10 +13283,14 @@ const options = Object(react_intl__WEBPACK_IMPORTED_MODULE_3__["defineMessages"]
     "id": "tw.accent.rainbow",
     "defaultMessage": "Rainbow"
   },
-  // Added Yellow option below
   [_lib_themes_index_js__WEBPACK_IMPORTED_MODULE_8__["ACCENT_YELLOW"]]: {
     "id": "tw.accent.yellow",
     "defaultMessage": "Yellow"
+  },
+  // Added Green option below
+  [ACCENT_GREEN]: {
+    "id": "tw.accent.green",
+    "defaultMessage": "Green"
   }
 });
 const icons = {
