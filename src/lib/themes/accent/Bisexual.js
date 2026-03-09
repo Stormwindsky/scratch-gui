568a77c251ffdea43296cabb44d446178a03c96c
Stormwindsky <83926426+Stormwindsky@users.noreply.github.com>
2026-03-09 17:58:51 -0400

    Add files via upload

---
+++ src/lib/themes/accent/Bisexual.js
@@ -0,0 +1,31 @@
+const guiColors = {
+    // Thème bisexuel : Rose, Violet, Bleu
+    'motion-primary': '#D60270',
+    'motion-primary-transparent': '#D60270e6',
+    'motion-tertiary': '#9B4F96',
+
+    'looks-secondary': '#D60270',
+    'looks-transparent': '#D6027059',
+    'looks-light-transparent': '#D6027026',
+    'looks-secondary-dark': '#0038A8',
+
+    'extensions-primary': '#9B4F96',
+    'extensions-tertiary': '#0038A8',
+    'extensions-transparent': 'rgba(155, 79, 150, 0.35)',
+    'extensions-light': '#f4c7e6', // Une version claire du rose pour les fonds
+
+    'drop-highlight': '#D60270',
+
+    // Dégradé inspiré du drapeau bisexuel
+    'menu-bar-background-image': 'linear-gradient(90deg, #D60270 0%, #D60270 40%, #9B4F96 40%, #9B4F96 60%, #0038A8 60%, #0038A8 100%)'
+};
+
+const blockColors = {
+    checkboxActiveBackground: '#D60270',
+    checkboxActiveBorder: '#9B4F96'
+};
+
+export {
+    guiColors,
+    blockColors
+};
