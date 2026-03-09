d1df32326ff56fb01faba5a393846e404722b274
Stormwindsky <83926426+Stormwindsky@users.noreply.github.com>
2026-03-09 23:34:55 +0000

    Deploying to gh-pages from @ Stormwindsky/scratch-gui@64902c875eed486bad62490afcc0d40c90af6363 🚀

--- js/embed.js
+++ js/embed.js
@@ -13292,7 +13292,7 @@ const options = Object(react_intl__WEBPACK_IMPORTED_MODULE_3__["defineMessages"]
     "defaultMessage": "Green"
   },
   // Added bisesexual theme option below
-  [ACCENT_BIESEXUAL]: {
+  [_lib_themes_index_js__WEBPACK_IMPORTED_MODULE_8__["ACCENT_BIESEXUAL"]]: {
     "id": "tw.accent.biesexual",
     "defaultMessage": "Biesexual"
   }
