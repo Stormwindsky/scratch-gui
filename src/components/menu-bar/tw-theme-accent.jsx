3a93ef535028c73a97723d365c10a7d03495843c
Stormwindsky <83926426+Stormwindsky@users.noreply.github.com>
2026-03-09 21:00:52 -0400

    Update tw-theme-accent.jsx

--- src/components/menu-bar/tw-theme-accent.jsx
+++ src/components/menu-bar/tw-theme-accent.jsx
@@ -8,7 +8,7 @@ import check from './check.svg';
 import dropdownCaret from './dropdown-caret.svg';
 import {MenuItem, Submenu} from '../menu/menu.jsx';
 // Added ACCENT_YELLOW to the imports below
-import {ACCENT_BLUE, ACCENT_MAP, ACCENT_PURPLE, ACCENT_RED, ACCENT_RAINBOW, ACCENT_YELLOW, ACCENT_GREEN, ACCENT_BIESEXUAL, Theme} from '../../lib/themes/index.js';
+import {ACCENT_BLUE, ACCENT_MAP, ACCENT_PURPLE, ACCENT_RED, ACCENT_RAINBOW, ACCENT_YELLOW, ACCENT_GREEN, ACCENT_BISEXUAL, Theme} from '../../lib/themes/index.js';
 import {openAccentMenu, accentMenuOpen, closeSettingsMenu} from '../../reducers/menus.js';
 import {setTheme} from '../../reducers/theme.js';
 import {persistTheme} from '../../lib/themes/themePersistance.js';
