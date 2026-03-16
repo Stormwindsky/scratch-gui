bdfc2d8c036c62f1997e65a58f24c2087e05374a
Stormwindsky <83926426+Stormwindsky@users.noreply.github.com>
2026-03-16 06:20:31 -0400

    Update index.js

--- src/lib/default-project/index.js
+++ src/lib/default-project/index.js
@@ -1,25 +1,29 @@
 import projectData from './project-data';
+
+/* eslint-disable import/no-unresolved */
 import overrideDefaultProject from '!arraybuffer-loader!./override-default-project.sb3';
 import backdrop from '!raw-loader!./cd21514d0531fdffb22204e0ec5ed84a.svg';
-import defaultZnak from '!raw-loader!./Znak.svg';
+import costume1 from '!raw-loader!./Znak.svg';
+/* eslint-enable import/no-unresolved */
 import {TextEncoder} from '../tw-text-encoder';
 
-const defaultProject = async (translator) => {
+const defaultProject = translator => {
     if (overrideDefaultProject.byteLength > 0) {
-        return [{ id: 0, assetType: 'Project', dataFormat: 'JSON', data: overrideDefaultProject }];
+        return [{
+            id: 0,
+            assetType: 'Project',
+            dataFormat: 'JSON',
+            data: overrideDefaultProject
+        }];
     }
 
-    const encoder = new TextEncoder();
-    const savedKustom = JSON.parse(localStorage.getItem('kustoms_config') || '{}');
-    
-    // Si une URL est définie, on la récupère, sinon on utilise le fichier par défaut
-    let svgData = defaultZnak;
-    if (savedKustom.spriteUrl) {
-        try {
-            const response = await fetch(savedKustom.spriteUrl);
-            svgData = await response.text();
-        } catch (e) { console.error("Could not load custom SVG", e); }
+    let _TextEncoder;
+    if (typeof TextEncoder === 'undefined') {
+        _TextEncoder = require('text-encoding').TextEncoder;
+    } else {
+        _TextEncoder = TextEncoder;
     }
+    const encoder = new _TextEncoder();
 
     const projectJson = projectData(translator);
     return [{
@@ -36,7 +40,8 @@ const defaultProject = async (translator) => {
         id: '927d672925e7b99f7813735c484c6922',
         assetType: 'ImageVector',
         dataFormat: 'SVG',
-        data: encoder.encode(svgData)
+        data: encoder.encode(costume1)
     }];
 };
+
 export default defaultProject;
