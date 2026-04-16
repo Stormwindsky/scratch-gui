99d1486bd13b20b488a8464b18aaca7c5aa1debd
Stormwindsky <83926426+Stormwindsky@users.noreply.github.com>
2026-04-16 23:55:39 +0000

    Deploying to gh-pages from @ Stormwindsky/scratch-gui@e61b45eb8d195e2762eb0cf72ca0cfb0ef943b78 🚀

--- js/embed.js
+++ js/embed.js
@@ -38496,6 +38496,12 @@ __webpack_require__.r(__webpack_exports__);
 }, {
   tag: 'patterns',
   intlLabel: _tag_messages_js__WEBPACK_IMPORTED_MODULE_0__["default"].patterns
+}, {
+  tag: 'opengameart',
+  intlLabel: _tag_messages_js__WEBPACK_IMPORTED_MODULE_0__["default"].opengameart
+}, {
+  tag: 'stormwindsky',
+  intlLabel: _tag_messages_js__WEBPACK_IMPORTED_MODULE_0__["default"].stormwindsky
 }]);
 
 /***/ }),
