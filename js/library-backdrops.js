5d27788e35f43aed304cfee8bd8696f9960311c3
Stormwindsky <83926426+Stormwindsky@users.noreply.github.com>
2026-04-16 15:14:35 +0000

    Deploying to gh-pages from @ Stormwindsky/scratch-gui@9ab951a14bee842e6bedb4e6e0e9f24e4d7a3f89 🚀

--- js/library-backdrops.js
+++ js/library-backdrops.js
@@ -7,7 +7,7 @@
 /*! exports provided: 0, 1, default */
 /***/ (function(module) {
 
-module.exports = JSON.parse("[{\"name\":\"kitchen Background (items)\",\"tags\":[\"cc0\"],\"assetId\":\"21e4c3efbf376e69b2eb2fcbc0f1af80\",\"bitmapResolution\":2,\"dataFormat\":\"png\",\"md5ext\":\"21e4c3efbf376e69b2eb2fcbc0f1af80.png\",\"rotationCenterX\":240,\"rotationCenterY\":180},{\"name\":\"kitchen Background\",\"tags\":[\"cc0\"],\"assetId\":\"f0dd7c357e570ee886a78418231ce85d\",\"bitmapResolution\":2,\"dataFormat\":\"png\",\"md5ext\":\"f0dd7c357e570ee886a78418231ce85d.png\",\"rotationCenterX\":240,\"rotationCenterY\":180}]");
+module.exports = JSON.parse("[{\"name\":\"Space1\",\"tags\":[\"OpenGameArt\"],\"assetId\":\"81801dd92eb894a0d343657957d1753d\",\"bitmapResolution\":2,\"dataFormat\":\"png\",\"md5ext\":\"81801dd92eb894a0d343657957d1753d.png\",\"rotationCenterX\":240,\"rotationCenterY\":180},{\"name\":\"glitch backdrops (4 fun lol)\",\"tags\":[\"glitch\"],\"assetId\":\"f0dd7c357e570ee886a78418231ce85d\",\"bitmapResolution\":2,\"dataFormat\":\"png\",\"md5ext\":\"f0dd7c357e570ee886a78418231ce85d.png\",\"rotationCenterX\":240,\"rotationCenterY\":180}]");
 
 /***/ })
 
