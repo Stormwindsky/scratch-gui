86102dfd1746f01d44f6dd5302314171200a4617
Stormwindsky <83926426+Stormwindsky@users.noreply.github.com>
2026-04-16 23:53:05 +0000

    Deploying to gh-pages from @ Stormwindsky/scratch-gui@06e52d3abf543e8d316f8966bf506ce6c910afb2 🚀

--- js/library-backdrops.js
+++ js/library-backdrops.js
@@ -7,7 +7,7 @@
 /*! exports provided: 0, 1, default */
 /***/ (function(module) {
 
-module.exports = JSON.parse("[{\"name\":\"Space1\",\"tags\":[\"OpenGameArt\"],\"assetId\":\"81801dd92eb894a0d343657957d1753d\",\"bitmapResolution\":2,\"dataFormat\":\"png\",\"md5ext\":\"81801dd92eb894a0d343657957d1753d.png\",\"rotationCenterX\":240,\"rotationCenterY\":180},{\"name\":\"glitch backdrops (4 fun lol)\",\"tags\":[\"glitch\"],\"assetId\":\"f0dd7c357e570ee886a78418231ce85d\",\"bitmapResolution\":2,\"dataFormat\":\"png\",\"md5ext\":\"f0dd7c357e570ee886a78418231ce85d.png\",\"rotationCenterX\":240,\"rotationCenterY\":180}]");
+module.exports = JSON.parse("[{\"name\":\"Space1\",\"tags\":[\"OpenGameArt\"],\"assetId\":\"81801dd92eb894a0d343657957d1753d\",\"bitmapResolution\":2,\"dataFormat\":\"png\",\"md5ext\":\"81801dd92eb894a0d343657957d1753d.png\",\"rotationCenterX\":480,\"rotationCenterY\":360},{\"name\":\"glitch backdrops (4 fun lol)\",\"tags\":[\"glitch\"],\"assetId\":\"bruh\",\"bitmapResolution\":2,\"dataFormat\":\"png\",\"md5ext\":\"bruh.png\",\"rotationCenterX\":480,\"rotationCenterY\":360}]");
 
 /***/ })
 
