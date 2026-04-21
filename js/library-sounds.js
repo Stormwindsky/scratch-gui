83120bd9fa36fdfdcf0a3f87bfd114ff4b52ccc6
Stormwindsky <83926426+Stormwindsky@users.noreply.github.com>
2026-04-21 21:34:44 +0000

    Deploying to gh-pages from @ Stormwindsky/scratch-gui@034caae82b86798e96ad659b8b2024a95bdb2a1b 🚀

--- js/library-sounds.js
+++ js/library-sounds.js
@@ -7,7 +7,7 @@
 /*! exports provided: 0, 1, default */
 /***/ (function(module) {
 
-module.exports = JSON.parse("[{\"name\":\"Bumbledee under pressure\",\"tags\":[\"music\",\"classical\",\"trailer\"],\"assetId\":\"b4f4a6ce4f4bfa42f468e18806af8474\",\"dataFormat\":\"\",\"md5ext\":\"b4f4a6ce4f4bfa42f468e18806af8474.wav\",\"sampleCount\":56320,\"rate\":44100},{\"name\":\"A Elec Bass\",\"tags\":[\"music\",\"instruments\",\"notes\"],\"assetId\":\"5cb46ddd903fc2c9976ff881df9273c9\",\"dataFormat\":\"\",\"md5ext\":\"5cb46ddd903fc2c9976ff881df9273c9.wav\",\"sampleCount\":11840,\"rate\":44100}]");
+module.exports = JSON.parse("[{\"name\":\"Bumbledee under pressure\",\"tags\":[\"music\",\"classical\",\"trailer\"],\"assetId\":\"b4f4a6ce4f4bfa42f468e18806af8474\",\"dataFormat\":\"\",\"md5ext\":\"b4f4a6ce4f4bfa42f468e18806af8474.wav\",\"sampleCount\":3921372,\"rate\":44100},{\"name\":\"A Elec Bass\",\"tags\":[\"music\",\"instruments\",\"notes\"],\"assetId\":\"5cb46ddd903fc2c9976ff881df9273c9\",\"dataFormat\":\"\",\"md5ext\":\"5cb46ddd903fc2c9976ff881df9273c9.wav\",\"sampleCount\":11840,\"rate\":44100}]");
 
 /***/ })
 
