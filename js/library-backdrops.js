0829c6a050b387ac125585ce2df2351fab3db01c
Stormwindsky <83926426+Stormwindsky@users.noreply.github.com>
2026-04-21 20:47:53 +0000

    Deploying to gh-pages from @ Stormwindsky/scratch-gui@36ba990b05ff11b60dad9ebb494578981de1f013 🚀

--- js/library-backdrops.js
+++ js/library-backdrops.js
@@ -7,7 +7,7 @@
 /*! exports provided: 0, 1, default */
 /***/ (function(module) {
 
-module.exports = JSON.parse("[{\"name\":\"Space1\",\"tags\":[\"OpenGameArt\"],\"assetId\":\"5de4956ca2e620317fb7bd39b3abc910\",\"bitmapResolution\":2,\"dataFormat\":\"png\",\"md5ext\":\"5de4956ca2e620317fb7bd39b3abc910.png\",\"rotationCenterX\":480,\"rotationCenterY\":360},{\"name\":\"Gliṭcȟ\",\"tags\":[\"glitch\"],\"assetId\":\"bruh\",\"bitmapResolution\":2,\"dataFormat\":\"png\",\"md5ext\":\"bruh.png\",\"rotationCenterX\":480,\"rotationCenterY\":360}]");
+module.exports = JSON.parse("[{\"name\":\"Space1\",\"tags\":[\"space\"],\"assetId\":\"5de4956ca2e620317fb7bd39b3abc910\",\"bitmapResolution\":2,\"dataFormat\":\"png\",\"md5ext\":\"5de4956ca2e620317fb7bd39b3abc910.png\",\"rotationCenterX\":480,\"rotationCenterY\":360},{\"name\":\"Gliṭcȟ\",\"tags\":[\"glitch\"],\"assetId\":\"bruh\",\"bitmapResolution\":2,\"dataFormat\":\"png\",\"md5ext\":\"bruh.png\",\"rotationCenterX\":480,\"rotationCenterY\":360}]");
 
 /***/ })
 
